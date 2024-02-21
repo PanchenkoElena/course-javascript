bb594297992b1bf5f0193dccc8190ddc9f09d567
Elena Panchenko <Panchenko888@bk.ru>
2024-02-21 17:50:59 +0700

    выполнено домашнее задание №1

--- projects/functions/index.js
+++ projects/functions/index.js
@@ -12,8 +12,9 @@
 
  Другими словами: функция должна возвращать в неизменном виде то, что поступает ей на вход
  */
-function returnFirstArgument() {}
-
+function returnFirstArgument(arg) {
+  return arg;
+}
 /*
  Задание 2:
 
@@ -28,8 +29,9 @@ function returnFirstArgument() {}
  Пример:
    sumWithDefaults(10) вернет 110
  */
-function sumWithDefaults() {}
-
+function sumWithDefaults(a, b = 100) {
+  return a + b;
+}
 /*
  Задание 3:
 
@@ -38,8 +40,9 @@ function sumWithDefaults() {}
  Пример:
    returnFnResult(() => 'привет') вернет 'привет'
  */
-function returnFnResult() {}
-
+function returnFnResult(func) {
+  return func();
+}
 /*
  Задание 4:
 
@@ -53,8 +56,13 @@ function returnFnResult() {}
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter() {}
-
+function returnCounter(i = 0) {
+  function bar() {
+    i = i + 1;
+    return i;
+  }
+  return bar;
+}
 /*
  Задание 5 *:
 
@@ -64,7 +72,9 @@ function returnCounter() {}
  Пример:
    returnArgumentsArray(1, 2, 3) вернет [1, 2, 3]
  */
-function returnArgumentsArray() {}
+function returnArgumentsArray(...arg) {
+  return arg;
+}
 
 export {
   returnFirstArgument,
